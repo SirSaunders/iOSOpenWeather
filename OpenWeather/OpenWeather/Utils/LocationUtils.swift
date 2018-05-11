5d698ffedccd1742f6df2d8c1251e62e76fd4e3a
Johnathan Saunders <Saundersokj@gmail.com>
2018-05-11 00:30:10 -0400

    fixed problem that could cause app to not work on first launch

--- OpenWeather/OpenWeather/Utils/LocationUtils.swift
+++ OpenWeather/OpenWeather/Utils/LocationUtils.swift
@@ -22,25 +22,31 @@ func getCurrentLocation() -> (longitude:Double,latitude:Double){
     if( CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedWhenInUse ||
         CLLocationManager.authorizationStatus() != CLAuthorizationStatus.denied){
         currentLocation = locManager.location
+        if(currentLocation != nil){
         return  (currentLocation.coordinate.longitude, currentLocation.coordinate.latitude)
-    }else{
-        print("could not get location")
-        return (0.0,0.0) //error getting current loaction return 0.0, 0.0 as default
+        }
     }
-    
+        print("could not get location")
+        return (40.7,74.0) //error getting current loaction return 0.0, 0.0 as default
 }
 
 /*
  * @return the postal as String code given a longitude and latitude
  */
 func getPostalCodeFromCoordinates(latitude: Double, longitude: Double,completionHandler:  @escaping (String) -> Void){
+    print(latitude)
+    print(longitude)
     let location = CLLocation(latitude: latitude, longitude: longitude )
     //use CLGeocoder to get readable location information
     CLGeocoder().reverseGeocodeLocation(location, completionHandler: { (placemarks, error) -> Void in
         var placeMark: CLPlacemark!
         placeMark = placemarks?[0]
         //return postal code
+        if(placeMark != nil && placeMark.postalCode != nil){
         completionHandler(placeMark.postalCode!)
+        }else{
+            completionHandler("10001")
+        }
         //todo: error is not handled, could cause a lock up as this function may never complete in some cases.
     })
 }
